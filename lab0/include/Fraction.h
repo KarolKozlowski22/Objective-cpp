6ab3975a34f3899668860892bd67bd12edfe1242
KarolKozlowski22 <kozlowskikarol02@gmail.com>
2023-05-05 00:00:33 +0200

    Zatwierdzenie lokalnych zmian

--- lab0/include/Fraction.h
+++ lab0/include/Fraction.h
@@ -3,22 +3,40 @@
 #pragma once 
 
 class Fraction{
+
     private:
+    //class members
     int _numerator;
     int _denominator;
+
     public:
+    //constructors
     Fraction(){};
     Fraction(const int, const int);
+
+    //operator double in order to convert to double
     operator double()const{
         return static_cast<double>(_numerator)/static_cast<double>(_denominator);
     }
+
+    //void function simplify in order to simplify the fraction
     void simplify();
+
+    //operator * in order to multiply two fractions
     Fraction operator*(const Fraction &)const;
+
+    //operator = in order to assign a fraction to another
     Fraction & operator=(const Fraction & );
+
+    //operator + in order to add two fractions
     Fraction operator+(const Fraction &)const;
+
+    //operator * in order to multiply a fraction by an integer
     friend Fraction operator*(const int num, const Fraction & frac){
         return Fraction(num*frac._numerator, frac._denominator);
     }
+
+    //operator << in order to print a fraction
     friend std::ostream& operator<<(std::ostream & out, const Fraction & frac){
         return out << frac._numerator << "/" << frac._denominator;
     }
--- lab0/include/FractionArray.h
+++ lab0/include/FractionArray.h
@@ -3,10 +3,18 @@
 #pragma once
 
 class FractionArray{
+
     private:
+
+    //class members
     std::vector<Fraction> m_fractions;
+
     public:
+
+    //constructors
     FractionArray(size_t size):m_fractions(size){}
+
+    //operator [] in order to access the elements of the array
     Fraction & operator[](size_t index){
         if(index>m_fractions.size()){
             return m_fractions[m_fractions.size()-1];
@@ -16,6 +24,8 @@ class FractionArray{
         }
         
     }
+
+    //operator [] in order to access the elements of the array const version in order to access the elements of a const array
     const Fraction & operator[](size_t index)const{
         if(index>m_fractions.size()){
             return m_fractions[m_fractions.size()-1];
